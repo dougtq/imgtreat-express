06bb32f70ecbe3fa6926c2da6fb0e91123b51745
Douglas Alves <douglas.expalves@gmail.com>
2017-10-22 04:04:50 -0200

    Update server.js - resize image

--- server.js
+++ server.js
@@ -3,14 +3,20 @@
 const express = require('express')
 const multer = require('multer')
 const sharp = require('sharp')
+const fs = require('fs')
 let app = express()
 
 let upload = multer({ dest: 'assets/' })
 
-app.post('/resize/:height/:width', upload.single('image'), (req, res) => {
-  console.log(req.file)
-  
-  res.json('Hello World')
+app.post('/resize/:height/:width', upload.any(), (req, res) => {
+  let { height, width } = req.params
+
+  res.set('content-type', 'image/jpg')
+
+  sharp(`assets/iCloud.jpg`)
+    .resize(Number(width), Number(height))
+    .toFile(`assets/${req.file.filename}-${width}-${height}`)
+    .pipe(res)
 })
 
 app.listen(3000, err => {
